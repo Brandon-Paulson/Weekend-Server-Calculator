aab9be376a531c3a4ca26d08e5cf7c7135cd155b
Brandon Paulson <brandonpaulson28@gmail.com>
2023-05-14 17:38:56 -0500

    added in a function for using one of four mathmatic operators and returning a calculated value

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,24 @@
 console.log('Hey the server works!');
 
+function sendArithmetic(event) {
+    event.preventDefault();
+    let dataSet= {
+    inputOne: document.querySelector('#inputValueOne').value,
+    inputArithmetic: document.querySelector('#arithmetic').value,
+    inputTwo: document.querySelector('#inputValueTwo').value
+    }
+    console.log(dataSet);
+    console.log(dataSet.inputOne);
+    if (dataSet.inputArithmetic == '+' ) {
+        document.querySelector('#contentDiv').innerHTML += (Number(dataSet.inputOne) + Number(dataSet.inputTwo));
+    }
+    else if (dataSet.inputArithmetic == '-') {
+        document.querySelector('#contentDiv').innerHTML += (Number(dataSet.inputOne) - Number(dataSet.inputTwo));
+    }
+    else if (dataSet.inputArithmetic == '*') {
+        document.querySelector('#contentDiv').innerHTML += (Number(dataSet.inputOne) * Number(dataSet.inputTwo));
+    }
+    else {
+        document.querySelector('#contentDiv').innerHTML += (Number(dataSet.inputOne) / Number(dataSet.inputTwo));
+    }
+};
